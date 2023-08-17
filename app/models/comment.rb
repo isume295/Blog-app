a11f78cbc930734fb6464107af2510780b86b82b
Sumeya <sumeyaibrahim72@gmail.com>
2023-08-17 14:50:01 +0300

    Fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,14 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    attribute :text, :text
+  attribute :text, :text
 
-    # Callbacks
-    after_create :update_post_comments_counter
-    after_destroy :update_post_comments_counter
+  # Callbacks
+  after_create :update_post_comments_counter
+  after_destroy :update_post_comments_counter
 
-    def update_post_comments_counter
-        post.update(comments_count: post.comments.count)
-    end
-end 
+  def update_post_comments_counter
+    post.update(comments_count: post.comments.count)
+  end
+end
