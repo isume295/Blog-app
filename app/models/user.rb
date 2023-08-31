3160c95b273f81cdc8c949bb73d3ba6a36c759ab
Sumeya <sumeyaibrahim72@gmail.com>
2023-08-31 15:11:05 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   has_many :comments
   has_many :likes
   has_many :posts, foreign_key: 'author_id'
